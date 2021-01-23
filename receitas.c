8eb4d57e06645431c8e773671592c66e491c8c12
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2021-01-23 12:30:49 -0300

    codigo
    adicionei o código com as alterações já feitas anteriormente.

--- receitas.c
+++ receitas.c
@@ -1,1 +1,42 @@
-#include
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///            Projeto Caderno Virtual de Receitas Culinárias               ///
+///            Developed by  @Sana-El e @cintiacq - Jan, 2021               ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///            Projeto Caderno Virtual de Receitas Culinárias               ///\n");
+    printf("///            Developed by  @Sana-El e @cintiacq - Jan, 2021               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = Caderno Virtual de Receitas Culinárias = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  O projeto propõe um sistema capaz de gerenciar um conjunto de          ///\n");
+    printf("///  receitas culinárias, permitindo a busca por pratos que contenham       ///\n");
+    printf("///  determinados ingredientes e a respectiva otimização dos estoques.      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
