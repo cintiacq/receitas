152d24646f67e95bbc14bf06ff1caaa68bedd451
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2021-03-01 20:12:13 -0300

    Create moduloIngredientes.h

---
+++ moduloIngredientes.h
@@ -0,0 +1,7 @@
+void moduloIngrediente(void);
+char menuIngredientes(void);
+void adicionarIngrediente(void);
+void menuEditIngrediente(void);
+void listaingredientes(void);
+void apagaringredientes(void);
+void pesquisaringredientes(void);
