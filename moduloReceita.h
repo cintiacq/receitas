d6613aa91c8fa0c57d47e8036244a6daa1bade3f
Cintia CQ <60833304+cintiacq@users.noreply.github.com>
2021-03-01 20:13:10 -0300

    Create moduloReceita.h

---
+++ moduloReceita.h
@@ -0,0 +1,8 @@
+void moduloReceita(void);
+char menuReceita(void);
+void novaReceita(void);
+void delReceita(void);  
+void pesquisarReceita(void);
+void listareceitas(void); 
+void editReceita(void); 
+void menuEditNome(void);
