258dcceae3b8633fe2c09e553a954515a6ce4803
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2021-03-01 18:23:56 -0300

    Create ModuloSobre.c

---
+++ ModuloSobre.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuSobre(void) {
+    system("clear");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///            Projeto Caderno Virtual de Receitas Culinárias               ///\n");
+    printf("///            Developed by  @Sana-El e @cintiacq - Jan, 2021               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = Caderno Virtual de Receitas Culinárias = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  O projeto de Caderno Virtual de Receitas tem como objetivo facilitar   ///\n");
+    printf("///     facilitar a vida de pessoas comuns, permitindo uma organização      ///\n");
+    printf("///                  com maior eficácia e simplicidade.                     ///\n");
+    printf("///     Permitirá que o usuário crie novas receitas, edite as existentes,   ///\n");
+    printf("///           delete, procure por receitas com x ingredientes, e etc.       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+	  printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
