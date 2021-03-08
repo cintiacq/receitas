598674ad38f286e074715e437492870a8c11071d
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2021-03-08 20:22:02 -0300

    Update moduloReceita.c

--- moduloReceita.c
+++ moduloReceita.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include "validação.h"
 
 char menuReceita(void) { 
   char op; 
@@ -20,14 +22,14 @@ char menuReceita(void) {
 	printf("///           = = = = = = = =   Menu Receita  = = = = = = = =             ///\n");
 	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
 	printf("///                                                                       ///\n");
-	printf("///           1. Criar nova receita                                       ///\n");
+  printf("///           0. Encerra o programa                                       ///\n");	
+  printf("///           1. Criar nova receita                                       ///\n");
 	printf("///           2. Edite uma receita                                        ///\n");
 	printf("///           3. Deletar receita                                          ///\n");
 	printf("///           4. procurar receita por ingrediente                         ///\n");
   printf("///           5. listar receitas                                          ///\n");
-  printf("///           0. Encerra o programa                                       ///\n");
 	printf("///                                                                       ///\n");
-	printf("///           Escolha a opção desejada:");
+	printf("///           Escolha a opção desejada: \n");
   scanf("%c", &op);
 	getchar();
 	printf("///                                                                       ///\n");
@@ -35,13 +37,12 @@ char menuReceita(void) {
 	printf("\n");
   printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
-
   return op;
 }
 
 void novaReceita(void) {
   char nomeReceita[20];
-
+  int identificador;
     system("clear");
 	printf("\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
@@ -59,20 +60,36 @@ void novaReceita(void) {
 	printf("///           = = = = = = = =   nova receita  = = = = = = = =             ///\n");
 	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
 	printf("///                                                                       ///\n");
-  printf("///            adicionar nome da  receita: ");
+  printf("///                     adicionar nome da  receita: ");
   scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nomeReceita);
   getchar();
+  while (validaNome(nomeReceita)==0){
+  printf("Deu erro, tente novamente \n");
+  printf("///                     adicionar nome da  receita: ");
+  scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nomeReceita);
+  getchar();
+  } 
+  if (validaNome(nomeReceita)==1) {
+  printf("///                   adicionar número da  receita: ");
+  scanf("%d", &identificador);
+  getchar();
+  while (validaIdentificador(&identificador) == 0){
+  printf("digite apenas numeros maiores que zero\n");
+  printf("///                   adicionar número da  receita: ");
+  scanf("%d", &identificador);
+  getchar();
+    }
+  }
   printf("///          ====================================================         ///\n");
   printf("///                                                                       ///\n");
 	printf("///                  Nome adicionado com sucesso!                         ///\n");
   printf("///                  Nome: %s \n", nomeReceita);
-	printf("///                                                                       ///\n");
+	printf("///                  Numero: %d \n", identificador);
+  printf("///                                                                       ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
   getchar();
 	printf("\n");
   printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-
-
 }
 
 void editReceita(void) {
@@ -98,9 +115,22 @@ void editReceita(void) {
 	printf("///          Digite o nome da receita que quer alterar: ");
 	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
 	getchar();
+  while (validaNome(nome)==0){
+  printf("Deu erro, tente novamente \n");
+  printf("///          Digite o nome da receita que quer alterar: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+  getchar();
+  } 
 	printf("///          Digite o novo nome: ");
 	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", novonome);
   getchar();
+  while (validaNome(novonome)==0){
+  printf("Deu erro, tente novamente \n");
+  printf("///          Digite o novo nome: ");
+	scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", novonome);
+  getchar();
+  }
+  printf("Deu erro, tente novamente \n");
   printf("///          ====================================================         ///\n");
   printf("///                                                                       ///\n");
 	printf("///                  Receita editada com sucesso!                         ///\n");
@@ -112,9 +142,10 @@ void editReceita(void) {
   getchar();
   printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 }
-
+//validação
 void menuEditIngredientes(void){
  char editingredientes[200];
+ int identificador; 
  system("clear");
   printf("/////////////////////////////////////////////////////////////////////////////\n");
 	printf("///                                                                       ///\n");
@@ -131,9 +162,24 @@ void menuEditIngredientes(void){
 	printf("///           = = = = Editar Ingredientes da receita  = = = =             ///\n");
 	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
 	printf("///                                                                       ///\n");
-	printf("///            Alterar ingredientes da receita: ");
+	printf("///            informe a receita ");
   scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ],[0-9/]", editingredientes);
   getchar();
+  while (validaNome(editingredientes)==0){
+  printf("Digite a primeira letra em maiúsculoo.");
+  printf("///            informe a receita ");
+  scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ],[0-9/]", editingredientes);
+  getchar();
+  }
+  printf("///            informe o numero da receita ");
+  scanf("%d", &identificador);
+  getchar();
+  while (validaIdentificador(&identificador) == 0){
+  printf("digite apenas numeros maiores que zero\n");
+  printf("///                   adicionar número da  receita: ");
+  scanf("%d", &identificador);
+  getchar();
+  }  
   printf("///          ====================================================         ///\n");
   printf("///                                                                       ///\n");
 	printf("///                  Receita editada com sucesso!                         ///\n");
@@ -144,8 +190,11 @@ void menuEditIngredientes(void){
   printf("\n");
 	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 }
+//validação
+
 void delReceita(void) {
   char del [20];
+  int identificador;
     system("clear");
 	printf("\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
@@ -166,6 +215,18 @@ void delReceita(void) {
 	printf("///          digite a receita a ser deletada: ");
   scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", del);
   getchar();
+  while (validaNome(del)==0){
+  printf("Deu erro, tente novamente \n");
+  printf("///            informe o numero da receita ");
+  scanf("%d", &identificador);
+  getchar();
+  }
+  while (validaIdentificador(&identificador) == 0){
+  printf("digite apenas numeros maiores que zero\n");
+  printf("///                   adicionar número da  receita: ");
+  scanf("%d", &identificador);
+  getchar();
+  }
   printf("///          ====================================================         ///\n");
   printf("///                                                                       ///\n");
 	printf("///                 Receita excluida com sucesso!                         ///\n");
@@ -177,6 +238,7 @@ void delReceita(void) {
   printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
   getchar();
 }
+//validação
 
 void pesquisarReceita(void) {
   char ingredientes[1000];
