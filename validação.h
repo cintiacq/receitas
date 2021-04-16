34c3cf2deb6d06ba24998beebe662812ac9ca64d
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2021-04-16 19:04:12 -0300

    Update validação.h

--- validação.h
+++ validação.h
@@ -1,3 +1,2 @@
 int validaNome (char nomeReceita[]);
-int validaIdentificador (int identificador[]);
-void limpaTela(void); //importado de https://github.com/flgorgonio/linguasolta/blob/main/util.h
+
