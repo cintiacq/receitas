2873b0f41a95aaf41c7d46c3213ed13ee14548db
Cintia CQ <60833304+cintiacq@users.noreply.github.com>
2021-04-16 19:10:34 -0300

    Delete moduloIngredientes.h

--- moduloIngredientes.h
+++
@@ -1,6 +0,0 @@
-void moduloIngrediente(void);
-char menuIngredientes(void);
-void adicionarIngrediente(void);
-void menuEditIngrediente(void);
-void apagaringredientes(void);
-void pesquisaringredientes(void);
